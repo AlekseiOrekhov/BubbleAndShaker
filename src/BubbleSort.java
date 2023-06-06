124b8d50c015c215ec86294be9651d847ab33851
AlekseiOrekhov <lexx.orekhov@mail.ru>
2023-06-06 14:21:36 +0300

    make sort faster because I lost specific moment of decreasing length of cycle for

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,16 +1,19 @@
 public class BubbleSort {
     public int[] sort(int[] input) {
         boolean isSorted = false;
+        int count = 0;
         while(!isSorted) {
             isSorted = true;
-            for (int i = 0; i < input.length - 1; i++) {
+            for (int i = 0; i < input.length - 1 - count; i++) {
                 if (input[i] > input[i+1]) {
                     isSorted = false;
                     int local = input[i+1];
                     input[i+1] = input[i];
                     input[i] = local;
+
                 }
             }
+            count++;
         }
         return input;
     }
--- src/ShakerSort.java
+++ src/ShakerSort.java
@@ -1,9 +1,10 @@
 public class ShakerSort {
     public int[] sort(int[] input) {
         boolean isSorted = false;
+        int count = 0;
         while(!isSorted) {
             isSorted = true;
-            for (int i = 0; i < input.length - 1; i++) {
+            for (int i = count; i < input.length - 1 - count; i++) {
                 if (input[i] > input[i+1]) {
                     isSorted = false;
                     int local = input[i+1];
@@ -14,13 +15,14 @@ public class ShakerSort {
             if (isSorted) {
                 return input;
             }
-            for (int i = input.length - 1; i > 0; i--) {
+            for (int i = input.length - 1 - count; i > count; i--) {
                 if (input[i] < input[i - 1]) {
                     int local = input[i - 1];
                     input[i - 1] = input[i];
                     input[i] = local;
                 }
             }
+            count++;
         }
         return input;
     }
